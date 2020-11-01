46cac834167bebb12e221d3970030fe731f41a0d
Dishant Arora <dishantarora@outlook.com>
2020-11-01 23:25:32 +0530

    Updated homepage stepdefinition file with following modifications:
    - Added regular expression support for feature file tagging
    - Added new then statement for homepage title verification.

--- web-automation/src/test/java/org/dishantarora/automation/web/stepdefinitions/Homepage.java
+++ web-automation/src/test/java/org/dishantarora/automation/web/stepdefinitions/Homepage.java
@@ -10,22 +10,27 @@ import io.cucumber.junit.Cucumber;
 @RunWith(Cucumber.class)
 public class Homepage {
 	
-	@Given("User has the homepage url")
+	@Given("^User has the homepage url.$")
 	public void user_has_the_homepage_url() {
 		System.out.println("Given Statement executed.");
 	}
 	
-	@When("User navigate to homepage url")
+	@When("^User navigate to homepage url.$")
 	public void user_navigate_to_homepage_url() {
 		System.out.println("When Statement executed.");
 	}
 	
-	@Then("Homepage is populated")
+	@Then("^Homepage is populated.$")
 	public void homepage_is_populated() {
 		System.out.println("Then Statement executed.");
 	}
 	
-	@Then("Pop-up is populated")
+	@Then("^Homepage title is \"(.*)\".$")
+	public void verify_homepage_title(String title) {
+		System.out.println("Homepage title is " + title);
+	}
+	
+	@Then("^Pop-up is populated.$")
 	public void pop_up_is_populated() {
 		System.out.println("Second Then Statement executed.");
 	}		
